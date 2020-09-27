9f6015c1b5c2f02338dfcc09f1543b445ffcaf3d
Matt Neal <impulseimg@gmail.com>
2020-09-27 16:41:30 -0400

    seeds

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Cheese Burger', FALSE),
+    ('Hamburger', FALSE),
+    ('Big Mac', FALSE),
+    ('Whopper', FALSE),
+    ('Invoke Burger', TRUE),
+    ('Travis Scott Burger', FALSE);
