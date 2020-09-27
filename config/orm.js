d604ec9ce7344d3bc86206b62622b25593bfa9ff
Matt Neal <impulseimg@gmail.com>
2020-09-27 17:28:22 -0400

    ORM

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,75 @@
 // Import MySQL connection.
+// O.R.M. where functions are for SQL database commands
 var connection = require("../config/connection.js");
 
+function printQuestionMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+    return arr.toString();
+}
+
+function objToSql(ob) {
+    var arr = [];
+
+    for (var key in ob) {
+        arr.push(key + "=" + ob[key]);
+    }
+
+    return arr.toString();
+}
+
+var orm = {
+    all: function (tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    // table = burgers, cols = columns we want to insert into, vals = array of values
+    create: function (table, cols, vals, cb) {
+        var queryString = "INSERT INTO" + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+
+    // table = burgers, objColVals = column values to update, condition
+    update: function (table, objColVals, condition, cb) {
+        var queryString = "UPDATE" + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    }
+};
 
 
 
