40a4cfa0fc2e64026cf4f359e0999b471d9a8094
Matt Neal <impulseimg@gmail.com>
2020-10-24 00:08:19 -0400

    server issue fix

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -20,7 +20,7 @@ router.post("/api/burgers", function (req, res) {
         res.status(200).end();
     });
 });
-this code allows the user to devour a burger, updated its status in the db
+// this code allows the user to devour a burger, updated its status in the db
 router.put("/api/burgers/:id", function (req, res) {
     const condition = { id: req.params.id };
 
@@ -39,18 +39,18 @@ router.put("/api/burgers/:id", function (req, res) {
 });
 
 
-router.delete("/api/burgers/:id", function (req, res) {
-    var condition = "id = " + req.params.id;
+// router.delete("/api/burgers/:id", function (req, res) {
+//     var condition = "id = " + req.params.id;
 
-    burger.delete(condition, function (result) {
-        if (result.affectedRows == 0) {
-            // If no rows were changed, then the ID must not exist, so 404
-            return res.status(404).end();
-        } else {
-            res.status(200).end();
-        }
-    });
-});
+//     burger.delete(condition, function (result) {
+//         if (result.affectedRows == 0) {
+//             // If no rows were changed, then the ID must not exist, so 404
+//             return res.status(404).end();
+//         } else {
+//             res.status(200).end();
+//         }
+//     });
+// });
 
 // Export routes for server.js to use.
 module.exports = router;
